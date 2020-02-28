ac94d57587b4e79b34052a8ad0e66cf685844906
RealNobody <RealNobody1@cox.net>
2020-02-27 20:23:21 -0700

    US4646 - Do not autosave taggable objects because they should already be saved.

--- lib/acts_as_taggable_on_mongoid/models/concerns/tagging_associations.rb
+++ lib/acts_as_taggable_on_mongoid/models/concerns/tagging_associations.rb
@@ -11,7 +11,11 @@ module ActsAsTaggableOnMongoid
 
           belongs_to :tag, counter_cache: true, inverse_of: :taggings
           belongs_to :tagger, polymorphic: true, optional: true
-          belongs_to :taggable, polymorphic: true
+
+          # Tags and Taggings are created in an after_save event on taggable.
+          # If autosave is not false, then this will cause the taggable object
+          # to be saved AGAIN, that could cause a number of unwanted side-effects.
+          belongs_to :taggable, polymorphic: true, autosave: false
 
           before_validation :atom_unset_blank_owner
         end
